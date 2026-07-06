d4d0d767015a3664b90fa94ecc1e8c1719b5f77e
İlhan Hesenzade <wkj7yy2pzd@privaterelay.appleid.com>
2026-07-06 03:01:50 +0300

    Add Message model

---
+++ backend/models/Message.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    recipient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    isRead: {
+      type: Boolean,
+      default: false
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Message', messageSchema);
